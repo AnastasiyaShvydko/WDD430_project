65a580708b1641c78be96a84c94a6019b9d7e5b5
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-10-26 00:46:26 -0400

    Update auth.config.ts

--- handcrafted-dashboard/auth.config.ts
+++ handcrafted-dashboard/auth.config.ts
@@ -14,7 +14,7 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/handcraft/product_edit', nextUrl));
+        return Response.redirect(new URL('/handcraft/catalog', nextUrl));
       }
       return true;
     },
