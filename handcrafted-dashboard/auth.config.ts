5ddbfa4d73dd2fc6b7301dbde3f2e7880c830917
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-10-26 00:53:38 -0400

    Update auth.config.ts

--- handcrafted-dashboard/auth.config.ts
+++ handcrafted-dashboard/auth.config.ts
@@ -9,7 +9,7 @@ export const authConfig = {
     
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/handcraft/catalog');
+      const isOnDashboard = nextUrl.pathname.startsWith('/handcraft/product_edit');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
