b0f82b648bd1e90b0fbdddc6658da65c38759c27
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-10-28 11:26:12 -0400

    Update ProductCard.tsx

--- handcrafted-dashboard/app/ui/components/ProductCard.tsx
+++ handcrafted-dashboard/app/ui/components/ProductCard.tsx
@@ -24,7 +24,7 @@ export default function ProductCard( { product }: { product: ProductByIdTable }
                     <div className="col-span-2">
                         <p className="text-xl">{product.title}</p>
                         <p>{product.description}</p>
-                        <p>${product.price}</p>
+                        <p>Price: ${product.price}</p>
                         <div><Link href={`/handcraft/product_by_id/${product.id}/product_by_id`} className="underline">Details</Link></div>
                         
                         {/* <div><Form product_id={product.id}/></div> */}
