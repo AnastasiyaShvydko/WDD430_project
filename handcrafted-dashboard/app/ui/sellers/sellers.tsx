84f82b92d94a9370ef0905fa8b4847b3a9c52cf3
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-10-29 11:55:12 -0400

    Update sellers.tsx

--- handcrafted-dashboard/app/ui/sellers/sellers.tsx
+++ handcrafted-dashboard/app/ui/sellers/sellers.tsx
@@ -25,8 +25,8 @@ return <>
                     </div>
                     <div className="col-span-2 justify-items-center">
                         <p className="text-xl">{seller.name}</p>
-                        <p className="p-5" >{seller.description}</p>
-                        <div className="p-5"><Link href={`/handcraft/products_by_seller/${seller.seller_code}/products_by_seller`} className="underline">See gallery</Link></div>
+                        <p className="pt-5 pb-5" >{seller.description}</p>
+                        <div className="pt-5 pb-5"><Link href={`/handcraft/products_by_seller/${seller.seller_code}/products_by_seller`} className="underline">See gallery</Link></div>
                         
                     </div>
                     
